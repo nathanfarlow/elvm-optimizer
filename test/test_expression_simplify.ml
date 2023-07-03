0f0e7201318132864c0dbcc4dd5d9215b55d9cba
Nathan Farlow <natethegreatest11@gmail.com>
2023-07-03 13:15:02 -0500

    Change to expect tests

--- test/test_expression_simplify.ml
+++ test/test_expression_simplify.ml
@@ -1,133 +1,113 @@
-open! Core
+open Core
 open Elvm_opt.Expression
 
-let%test "const remains same" =
-  let a = Const 0 in
-  equal a (simplify a)
+let print exp = print_s [%sexp (exp : t)]
 
-let%test "register remains same" =
-  let a = Register A in
-  equal a (simplify a)
+let%expect_test "const remains same" =
+  print @@ simplify (Const 0);
+  [%expect {| (Const 0) |}]
 
-let%test "getc remains same" =
-  let a = Getc in
-  equal a (simplify a)
+let%expect_test "register remains same" =
+  print @@ simplify (Register A);
+  [%expect {| (Register A) |}]
 
-let%test "memory address is simplified" =
-  let ugly = Memory (Sub (Const 1, Const 1)) in
-  let simplified = Memory (Const 0) in
-  equal simplified (simplify ugly)
+let%expect_test "getc remains same" =
+  print @@ simplify Getc;
+  [%expect {| Getc |}]
 
-let%test "add's elements are simplified" =
-  let ugly = Sub (Const 1, Const 1) in
-  let ugly = Add [ ugly; ugly ] in
-  let simplified = Const 0 in
-  equal simplified (simplify ugly)
+let%expect_test "memory address is simplified" =
+  print @@ simplify (Memory (Sub (Const 1, Const 1)));
+  [%expect {| (Memory (Const 0)) |}]
 
-let%test "add combines constants" =
-  let ugly = Add [ Const 1; Memory (Const 0); Const 2 ] in
-  let simplified = Add [ Const 3; Memory (Const 0) ] in
-  equal simplified (simplify ugly)
+let%expect_test "add's elements are simplified" =
+  print @@ simplify (Add [ Sub (Const 1, Const 1); Sub (Const 1, Const 1) ]);
+  [%expect {| (Const 0) |}]
 
-let%test "add simplifies nested adds" =
-  let ugly = Add [ Const 1; Add [ Const 2; Memory (Const 3) ]; Const 4 ] in
-  let simplified = Add [ Const 7; Memory (Const 3) ] in
-  equal simplified (simplify ugly)
+let%expect_test "add combines constants" =
+  print @@ simplify (Add [ Const 1; Memory (Const 0); Const 2 ]);
+  [%expect {| (Add ((Const 3) (Memory (Const 0)))) |}]
 
-let%test "add with one element is that element" =
-  let ugly = Add [ Const 1 ] in
-  let simplified = Const 1 in
-  equal simplified (simplify ugly)
+let%expect_test "add simplifies nested adds" =
+  print
+  @@ simplify (Add [ Const 1; Add [ Const 2; Memory (Const 3) ]; Const 4 ]);
+  [%expect {| (Add ((Const 7) (Memory (Const 3)))) |}]
 
-let%test "add simplifies singular nested add" =
-  let ugly = Add [ Add [ Const 1 ] ] in
-  let simplified = Const 1 in
-  equal simplified (simplify ugly)
+let%expect_test "add with one element is that element" =
+  print @@ simplify (Add [ Const 1 ]);
+  [%expect {| (Const 1) |}]
 
-let%test "sub is 0 when args are same" =
-  let ugly = Sub (Memory (Const 0), Memory (Const 0)) in
-  let simplified = Const 0 in
-  equal simplified (simplify ugly)
+let%expect_test "add simplifies singular nested add" =
+  print @@ simplify (Add [ Add [ Const 1 ] ]);
+  [%expect {| (Const 1) |}]
 
-let%test "sub evaluates constants" =
-  let ugly = Sub (Const 2, Const 1) in
-  let simplified = Const 1 in
-  equal simplified (simplify ugly)
+let%expect_test "sub is 0 when args are same" =
+  print @@ simplify (Sub (Memory (Const 0), Memory (Const 0)));
+  [%expect {| (Const 0) |}]
 
-let%test_unit "set simplifies children" =
-  let ugly =
-    Set { comparison = Eq; a = Sub (Const 1, Const 1); b = Memory (Const 0) }
-  in
-  let simplified = Set { comparison = Eq; a = Const 0; b = Memory (Const 0) } in
-  [%test_eq: bool] true (equal simplified (simplify ugly));
-  let ugly =
-    Set { comparison = Eq; a = Memory (Const 0); b = Sub (Const 1, Const 1) }
-  in
-  let simplified = Set { comparison = Eq; a = Memory (Const 0); b = Const 0 } in
-  [%test_eq: bool] true (equal simplified (simplify ugly))
+let%expect_test "sub evaluates constants" =
+  print @@ simplify (Sub (Const 2, Const 1));
+  [%expect {| (Const 1) |}]
 
-let%test "set simplifies eq when args are equal" =
-  let ugly =
-    Set { comparison = Eq; a = Memory (Const 0); b = Memory (Const 0) }
-  in
-  let simplified = Const 1 in
-  equal simplified (simplify ugly)
+let%expect_test "set simplifies children" =
+  print
+  @@ simplify
+       (Set
+          { comparison = Eq; a = Sub (Const 1, Const 1); b = Memory (Const 0) });
+  [%expect {| (Set ((comparison Eq) (a (Const 0)) (b (Memory (Const 0))))) |}];
+  print
+  @@ simplify
+       (Set
+          { comparison = Eq; a = Memory (Const 0); b = Sub (Const 1, Const 1) });
+  [%expect {| (Set ((comparison Eq) (a (Memory (Const 0))) (b (Const 0)))) |}]
 
-let%test "set simplifies ne when args are equal" =
-  let ugly =
-    Set { comparison = Ne; a = Memory (Const 0); b = Memory (Const 0) }
-  in
-  let simplified = Const 0 in
-  equal simplified (simplify ugly)
+let%expect_test "set simplifies eq when args are equal" =
+  print
+  @@ simplify
+       (Set { comparison = Eq; a = Memory (Const 0); b = Memory (Const 0) });
+  [%expect {| (Const 1) |}]
 
-let%test "set simplifies lt when args are equal" =
-  let ugly =
-    Set { comparison = Lt; a = Memory (Const 0); b = Memory (Const 0) }
-  in
-  let simplified = Const 0 in
-  equal simplified (simplify ugly)
+let%expect_test "set simplifies ne when args are equal" =
+  print
+  @@ simplify
+       (Set { comparison = Ne; a = Memory (Const 0); b = Memory (Const 0) });
+  [%expect {| (Const 0) |}]
 
-let%test "set simplifies le when args are equal" =
-  let ugly =
-    Set { comparison = Le; a = Memory (Const 0); b = Memory (Const 0) }
-  in
-  let simplified = Const 1 in
-  equal simplified (simplify ugly)
+let%expect_test "set simplifies lt when args are equal" =
+  print
+  @@ simplify
+       (Set { comparison = Lt; a = Memory (Const 0); b = Memory (Const 0) });
+  [%expect {| (Const 0) |}]
 
-let%test_unit "set simplifies eq when args are const" =
-  let ugly = Set { comparison = Eq; a = Const 0; b = Const 0 } in
-  let simplified = Const 1 in
-  [%test_eq: bool] true (equal simplified (simplify ugly));
-  let ugly = Set { comparison = Eq; a = Const 0; b = Const 1 } in
-  let simplified = Const 0 in
-  [%test_eq: bool] true (equal simplified (simplify ugly))
+let%expect_test "set simplifies le when args are equal" =
+  print
+  @@ simplify
+       (Set { comparison = Le; a = Memory (Const 0); b = Memory (Const 0) });
+  [%expect {| (Const 1) |}]
 
-let%test_unit "set simplifies ne when args are const" =
-  let ugly = Set { comparison = Ne; a = Const 0; b = Const 0 } in
-  let simplified = Const 0 in
-  [%test_eq: bool] true (equal simplified (simplify ugly));
-  let ugly = Set { comparison = Ne; a = Const 0; b = Const 1 } in
-  let simplified = Const 1 in
-  [%test_eq: bool] true (equal simplified (simplify ugly))
+let%expect_test "set simplifies eq when args are const" =
+  print @@ simplify (Set { comparison = Eq; a = Const 0; b = Const 0 });
+  [%expect {| (Const 1) |}];
+  print @@ simplify (Set { comparison = Eq; a = Const 1; b = Const 0 });
+  [%expect {| (Const 0) |}]
 
-let%test_unit "set simplifies lt when args are const" =
-  let ugly = Set { comparison = Lt; a = Const 0; b = Const 0 } in
-  let simplified = Const 0 in
-  [%test_eq: bool] true (equal simplified (simplify ugly));
-  let ugly = Set { comparison = Lt; a = Const 1; b = Const 0 } in
-  let simplified = Const 0 in
-  [%test_eq: bool] true (equal simplified (simplify ugly));
-  let ugly = Set { comparison = Lt; a = Const 0; b = Const 1 } in
-  let simplified = Const 1 in
-  [%test_eq: bool] true (equal simplified (simplify ugly))
+let%expect_test "set simplifies ne when args are const" =
+  print @@ simplify (Set { comparison = Ne; a = Const 0; b = Const 0 });
+  [%expect {| (Const 0) |}];
+  print @@ simplify (Set { comparison = Ne; a = Const 1; b = Const 0 });
+  [%expect {| (Const 1) |}]
 
-let%test_unit "set simplifies le when args are const" =
-  let ugly = Set { comparison = Le; a = Const 0; b = Const 0 } in
-  let simplified = Const 1 in
-  [%test_eq: bool] true (equal simplified (simplify ugly));
-  let ugly = Set { comparison = Le; a = Const 1; b = Const 0 } in
-  let simplified = Const 0 in
-  [%test_eq: bool] true (equal simplified (simplify ugly));
-  let ugly = Set { comparison = Le; a = Const 0; b = Const 1 } in
-  let simplified = Const 1 in
-  [%test_eq: bool] true (equal simplified (simplify ugly))
+let%expect_test "set simplifies lt when args are const" =
+  print @@ simplify (Set { comparison = Lt; a = Const 0; b = Const 0 });
+  [%expect {| (Const 0) |}];
+  print @@ simplify (Set { comparison = Lt; a = Const 1; b = Const 0 });
+  [%expect {| (Const 0) |}];
+  print @@ simplify (Set { comparison = Lt; a = Const 0; b = Const 1 });
+  [%expect {| (Const 1) |}]
+
+let%expect_test "set simplifies le when args are const" =
+  print @@ simplify (Set { comparison = Le; a = Const 0; b = Const 0 });
+  [%expect {| (Const 1) |}];
+  print @@ simplify (Set { comparison = Le; a = Const 1; b = Const 0 });
+  [%expect {| (Const 0) |}];
+  print @@ simplify (Set { comparison = Le; a = Const 0; b = Const 1 });
+  [%expect {| (Const 1) |}]
