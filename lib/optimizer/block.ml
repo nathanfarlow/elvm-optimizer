4bf084d49018562f40a08529934726a17c4241c7
Nathan Farlow <natethegreatest11@gmail.com>
2023-07-05 19:34:05 -0500

    Add references for block

--- lib/optimizer/block.ml
+++ lib/optimizer/block.ml
@@ -9,6 +9,17 @@ type t = {
 
 and branch = { primary : t; secondary : t option }
 
+(* todo: test when there are 2 cfg *)
+
+(* optimization ideas:
+    If this block has unconditional branch and is the only in edge to
+    target block, delete the jump instruction if one exists and
+    glue this block to the target, returning one block
+*)
+
 let optimize _t = failwith "not implemented"
 let find_living _ = failwith "find_living not implemented"
-let references _ = failwith "not implemented"
+
+let references t =
+  List.concat_map t.statements ~f:Statement.references
+  |> List.dedup_and_sort ~compare:String.compare
---
+++ test/test_block.ml
@@ -0,0 +1,19 @@
+open Core
+open Elvm_opt.Block
+
+let print_refs refs = print_s [%sexp (refs : string list)]
+let of_statements statements = { label = "foo"; statements; branch = None }
+
+let%expect_test "references returns statement references" =
+  of_statements
+    [
+      Assign { dst = Memory (Label "foo"); src = Label "bar" };
+      Assign { dst = Memory (Label "baz"); src = Label "baq" };
+    ]
+  |> references |> print_refs;
+  [%expect {| (baq bar baz foo) |}]
+
+let%expect_test "references removes duplicate references" =
+  of_statements [ Assign { dst = Memory (Label "foo"); src = Label "foo" } ]
+  |> references |> print_refs;
+  [%expect {| (foo) |}]
