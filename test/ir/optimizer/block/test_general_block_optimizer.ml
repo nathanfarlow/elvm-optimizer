34e7c11db08dbaa48bec71ec96b7795e531e3cff
Nathan Farlow <natethegreatest11@gmail.com>
2023-07-07 16:26:39 -0500

    Update general block optimizer tests

--- test/ir/optimizer/block/test_general_block_optimizer.ml
+++ test/ir/optimizer/block/test_general_block_optimizer.ml
@@ -7,7 +7,7 @@ module Block_opt = Elvm_opt.General_block_optimizer.Make (Statement_opt)
 let optimizer =
   Expression_opt.create () |> Statement_opt.create |> Block_opt.create
 
-let print result = print_s [%sexp (result : Block.t * bool)]
+let print_block result = print_s [%sexp (result : Block.M.t)]
 let optimize = Block_opt.optimize optimizer
 
 let%expect_test "optimizes statements" =
@@ -17,13 +17,14 @@ let%expect_test "optimizes statements" =
   in
   let in_edges = [] in
   let branch = None in
-  let block = Block.create ~label ~statements ~in_edges ~branch in
-  optimize block |> print;
+  let block = Block.M.{ label; statements; in_edges; branch } in
+  printf "%b" (optimize block);
+  [%expect "true"];
+  print_block block;
   [%expect
     {|
-    (((label foo) (statements ((Assign ((dst (Register A)) (src (Const 2))))))
-      (in_edges ()) (branch ()))
-     true) |}]
+    ((label foo) (statements ((Assign ((dst (Register A)) (src (Const 2))))))
+     (in_edges ()) (branch ())) |}]
 
 let%expect_test "corrects optimized branch" =
   let baz =
@@ -31,11 +32,11 @@ let%expect_test "corrects optimized branch" =
     let in_edges = [ Block.Edge.{ target = "foo"; type_ = Jump } ] in
     let statements = [ Statement.Nop ] in
     let branch = None in
-    Block.create ~label ~statements ~in_edges ~branch
+    Block.M.{ label; statements; in_edges; branch }
   in
   let bar =
     let in_edges = [ Block.Edge.{ target = "foo"; type_ = Fallthrough } ] in
-    Block.create ~label:"bar" ~statements:[ Nop ] ~in_edges ~branch:None
+    Block.M.{ label = "bar"; statements = [ Nop ]; in_edges; branch = None }
   in
   let foo =
     let statements =
@@ -50,16 +51,19 @@ let%expect_test "corrects optimized branch" =
       ]
     in
     let in_edges = [] in
-    let branch = Some (Block.Conditional_jump { true_ = baz; false_ = bar }) in
-    Block.create ~label:"foo" ~statements ~in_edges ~branch
+    let branch =
+      Some (Block.Branch.Conditional_jump { true_ = baz; false_ = bar })
+    in
+    Block.M.{ label = "foo"; statements; in_edges; branch }
   in
-  optimize foo |> print;
+  printf "%b" (optimize foo);
+  [%expect "true"];
+  print_block foo;
   [%expect
     {|
-    (((label foo) (statements ((Jump ((target (Label baz)) (condition ())))))
-      (in_edges ())
-      (branch
-       ((Unconditional_jump
-         ((label baz) (statements (Nop)) (in_edges (((target foo) (type_ Jump))))
-          (branch ()))))))
-     true) |}]
+    ((label foo) (statements ((Jump ((target (Label baz)) (condition ())))))
+     (in_edges ())
+     (branch
+      ((Unconditional_jump
+        ((label baz) (statements (Nop)) (in_edges (((target foo) (type_ Jump))))
+         (branch ())))))) |}]
