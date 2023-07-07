e0d4be68d53afbc17bb265e5408d8a2b6937e7a5
Nathan Farlow <natethegreatest11@gmail.com>
2023-07-07 16:44:58 -0500

    Update glue optimizer

---
+++ lib/ir/optimizer/block/glue_block_optimizer.ml
@@ -0,0 +1,79 @@
+open Core
+
+module Make (Reference_provider : Reference_provider_intf.S) = struct
+  type t = Reference_provider.t
+
+  let has_fallthrough_in_edge block =
+    let open Block.M in
+    List.exists block.in_edges ~f:(function
+      | { type_ = Fallthrough; _ } -> true
+      | _ -> false)
+
+  let rec optimize ref_provider block =
+    Inplace_optimizer_util.optimize_until_unchanging (optimize' ref_provider)
+      block
+
+  and optimize' ref_provider block =
+    let open Block.M in
+    match block.branch with
+    | Some (Unconditional_jump target)
+    (* if this block jumps unconditionally to target and target
+       does not have any fallthrough in edges, then we can change
+       the jump to fallthrough to target, saving a jump instruction *)
+      when not @@ has_fallthrough_in_edge target ->
+        ignore @@ optimize ref_provider target;
+        change_jump_to_fallthrough block target;
+        true
+    | Some (Fallthrough target)
+    (* if there are no references to target, it implies that target has only
+       one in-edge, which is this block. As a result, we can concatenate
+       these blocks *)
+      when not @@ Reference_provider.has_reference ref_provider target.label ->
+        ignore @@ optimize ref_provider target;
+        concatenate block target;
+        true
+    | _ ->
+        Block_optimizer_util.optimize_branch (optimize ref_provider)
+          block.branch
+
+  and change_jump_to_fallthrough block target =
+    (* update the target's in-edge for this block to be a fallthrough edge *)
+    target.in_edges <-
+      List.map target.in_edges ~f:(function
+        | { target; type_ = Fallthrough } as e
+          when String.equal target block.label ->
+            Block.Edge.{ e with type_ = Fallthrough }
+        | _ as e -> e);
+    (* update this block's branch to be fallthrough to target *)
+    target.branch <- Some (Block.Branch.Fallthrough target);
+    (* drop the jump instruction *)
+    block.statements <- block.statements |> List.drop_last_exn
+
+  and concatenate block target =
+    let open Block.M in
+    block.statements <- block.statements @ target.statements;
+    (* update this block's branch to be target's branch, but with
+       target's target's in-edges updated to this block *)
+    let update_in_edges targets_target =
+      targets_target.in_edges <-
+        List.map targets_target.in_edges ~f:(function
+          | { target = targets_target; _ } as e
+            when String.equal targets_target target.label ->
+              Block.Edge.{ e with target = block.label }
+          | _ as e -> e)
+    in
+    match target.branch with
+    | Some (Unconditional_jump target) ->
+        update_in_edges target;
+        block.branch <- Some (Block.Branch.Unconditional_jump target)
+    | Some (Conditional_jump { true_; false_ }) ->
+        update_in_edges true_;
+        update_in_edges false_;
+        block.branch <- Some (Conditional_jump { true_; false_ })
+    | Some (Fallthrough target) ->
+        update_in_edges target;
+        block.branch <- Some (Fallthrough target)
+    | None -> ()
+
+  let create = Fn.id
+end
--- lib/ir/optimizer/block/glue_block_optimizer.mli
+++ lib/ir/optimizer/block/glue_block_optimizer.mli
@@ -3,7 +3,7 @@ open! Core
 module Make (Reference_provider : Reference_provider_intf.S) : sig
   type t
 
-  include Optimizer_intf.S with type t := t and type target := Block.t
+  include Inplace_optimizer_intf.S with type t := t and type target := Block.M.t
 
   val create : Reference_provider.t -> t
 end
