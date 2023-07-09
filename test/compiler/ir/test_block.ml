fcf1d60fce1877148e4838473275a089c38186ff
Nathan Farlow <natethegreatest11@gmail.com>
2023-07-09 10:55:56 -0500

    Add block tests

---
+++ test/compiler/ir/test_block.ml
@@ -0,0 +1,23 @@
+open Core
+open Elvm.Compiler.Ir.Block
+
+let print_refs refs =
+  let sorted = Hash_set.to_list refs |> List.sort ~compare:String.compare in
+  print_s [%sexp (sorted : string list)]
+
+let of_statements statements =
+  { label = "foo"; statements; in_edges = []; branch = None }
+
+let%expect_test "references returns statement references" =
+  of_statements
+    [|
+      Assign { dst = Memory (Label "foo"); src = Label "bar" };
+      Assign { dst = Memory (Label "baz"); src = Label "baq" };
+    |]
+  |> references |> print_refs;
+  [%expect {| (baq bar baz foo) |}]
+
+let%expect_test "references removes duplicate references" =
+  of_statements [| Assign { dst = Memory (Label "foo"); src = Label "foo" } |]
+  |> references |> print_refs;
+  [%expect {| (foo) |}]
