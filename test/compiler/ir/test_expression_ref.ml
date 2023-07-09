9e19fb39ba9cc6ef92c36967721a212ccbfbaad6
Nathan Farlow <natethegreatest11@gmail.com>
2023-07-09 10:53:04 -0500

    Add expression references tests

---
+++ test/compiler/ir/test_expression_ref.ml
@@ -0,0 +1,30 @@
+open Core
+open Elvm.Compiler.Ir.Expression
+
+let print refs =
+  let sorted = Hash_set.to_list refs |> List.sort ~compare:String.compare in
+  print_s [%sexp (sorted : string list)]
+
+let%expect_test "label has reference" =
+  references (Label "foo") |> print;
+  [%expect {| (foo) |}]
+
+let%expect_test "memory has references" =
+  references (Memory (Label "foo")) |> print;
+  [%expect {| (foo) |}]
+
+let%expect_test "add has references" =
+  references (Add [ Label "foo"; Label "bar" ]) |> print;
+  [%expect {| (bar foo) |}]
+
+let%expect_test "sub has references" =
+  references (Sub (Label "foo", Label "bar")) |> print;
+  [%expect {| (bar foo) |}]
+
+let%expect_test "set has references" =
+  references (If { cmp = Eq; left = Label "foo"; right = Label "bar" }) |> print;
+  [%expect {| (bar foo) |}]
+
+let%expect_test "duplicate labels are removed" =
+  references (Add [ Label "foo"; Label "foo" ]) |> print;
+  [%expect {| (foo) |}]
