ade26174cddf2f379f4ca8e654b9b63041305d3f
Nathan Farlow <natethegreatest11@gmail.com>
2023-07-28 14:49:12 -0500

    Add ast statement optimizer mli

---
+++ lib/ast/optimizer/ast_statement_optimizer.mli
@@ -0,0 +1,9 @@
+module Make
+    (Expression_optimizer : Optimizer_intf.S
+                              with type target := Ast.Expression.t) : sig
+  type t
+
+  include Optimizer_intf.S with type t := t and type target := Ast_statement.t
+
+  val create : Expression_optimizer.t -> t
+end
