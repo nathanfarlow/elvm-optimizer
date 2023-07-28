62da858e2295c68e9ab1ecd83ac1748f02ef89fe
Nathan Farlow <natethegreatest11@gmail.com>
2023-07-28 16:46:05 -0500

    Add statement graph optimizer mli

--- lib/common/graph/optimizer/statement_graph_optimizer.ml
+++ lib/common/graph/optimizer/statement_graph_optimizer.ml
@@ -1,15 +1,7 @@
 module Make
     (Statement : Statement_intf.S)
-    (Optimizer : Optimizer_intf.S with type target := Statement.t) : sig
-  type t
-
-  include
-    Inplace_optimizer_intf.S
-      with type t := t
-       and type target := Statement.t Graph.t
-
-  val create : Optimizer.t -> t
-end = struct
+    (Optimizer : Optimizer_intf.S with type target := Statement.t) =
+struct
   type t = Optimizer.t
 
   let create = Fn.id
---
+++ lib/common/graph/optimizer/statement_graph_optimizer.mli
@@ -0,0 +1,12 @@
+module Make
+    (Statement : Statement_intf.S)
+    (Optimizer : Optimizer_intf.S with type target := Statement.t) : sig
+  type t
+
+  include
+    Inplace_optimizer_intf.S
+      with type t := t
+       and type target := Statement.t Graph.t
+
+  val create : Optimizer.t -> t
+end
