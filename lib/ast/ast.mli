1b2aac0c03ee299bf8081acbd5015a06350a3c5f
Nathan Farlow <natethegreatest11@gmail.com>
2023-07-28 14:44:20 -0500

    Add ast mli

---
+++ lib/ast/ast.mli
@@ -0,0 +1,35 @@
+module rec Expression : sig
+  type t =
+    | Const of int
+    | Label of string
+    | Var of Variable.t
+    | Add of t list
+    | Sub of t * t
+    | Getc
+    | If of Condition.t
+  [@@deriving sexp, equal, compare, hash]
+
+  include Propagator_exp_intf.S with type t := t and type var := Variable.t
+
+  val substitute : t -> from:t -> to_:t -> t * bool
+end
+
+and Comparison : sig
+  type t = Eq | Ne | Lt | Le [@@deriving sexp, equal, compare, hash]
+end
+
+and Condition : sig
+  type t = { cmp : Comparison.t; left : Expression.t; right : Expression.t }
+  [@@deriving sexp, equal, compare, hash]
+
+  val substitute : t -> from:Expression.t -> to_:Expression.t -> t * bool
+end
+
+and Variable : sig
+  type t = Register of Eir.Register.t | Memory of Expression.t
+  [@@deriving sexp, equal, compare, hash]
+
+  include Propagator_var_intf.S with type t := t
+
+  val substitute : t -> from:Expression.t -> to_:Expression.t -> t * bool
+end
