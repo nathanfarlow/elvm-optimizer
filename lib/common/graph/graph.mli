ccd855cf026567f565bf14f8dc1494e0298d0177
Nathan Farlow <natethegreatest11@gmail.com>
2023-07-28 16:43:43 -0500

    Fix types

--- lib/common/graph/graph.mli
+++ lib/common/graph/graph.mli
@@ -3,10 +3,9 @@ type 'a t = { nodes : (string, 'a Node.t) Base.Hashtbl.t }
 val create : (string, 'a Node.t) Base.Hashtbl.t -> 'a t
 val nodes : 'a t -> (string, 'a Node.t) Base.Hashtbl.t
 val find_blocks : 'a t -> (string, 'a Node.t) Base.Hashtbl.t
-val fresh_label : 'a -> 'b
-val register_node : 'a -> 'b -> 'c
+val fresh_label : 'a t -> string
+val register_node : 'a t -> 'a Node.t -> unit
 
 module For_tests : functor (Element : Sexpable) -> sig
-  val to_string : Element.t t
-  val find_terminals : 'a -> 'b -> string
+  val to_string : Element.t t -> string
 end
